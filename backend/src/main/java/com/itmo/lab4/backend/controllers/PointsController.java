49bc40e544b21a477f97b66ca6d8834708609126
kirphone <bulanovks@mail.ru>
2019-12-09 02:24:04 +0300

    Добавлен контроллер для получения конкретной точки по id

--- backend/src/main/java/com/itmo/lab4/backend/controllers/PointsController.java
+++ backend/src/main/java/com/itmo/lab4/backend/controllers/PointsController.java
@@ -2,7 +2,6 @@ package com.itmo.lab4.backend.controllers;
 
 import com.itmo.lab4.backend.database.PointRepository;
 import com.itmo.lab4.backend.database.UserRepository;
-import com.itmo.lab4.backend.database.entities.PointEntity;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -11,10 +10,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.servlet.ModelAndView;
 
-import java.util.Collection;
-import java.util.List;
+import java.util.stream.Collectors;
 
 import static org.springframework.http.ResponseEntity.ok;
 
@@ -27,11 +24,8 @@ public class PointsController {
 
     @GetMapping(path = "/{username}/points")
     public ResponseEntity getAll(@PathVariable("username") String username, Authentication auth){
-        return doAccess(username, auth, new MakeResponse() {
-            public ResponseEntity make() {
-                return ok(userRepository.findByUsername(username).getPoints());
-            }
-        });
+        return doAccess(username, auth,
+                () -> ok(userRepository.findByUsername(username).getPoints()));
     }
 
     interface MakeResponse{
@@ -45,9 +39,15 @@ public class PointsController {
         return response.make();
     }
 
-    /*@GetMapping(path = "/{username}/points/{point_id}")
+    @GetMapping(path = "/{username}/points/{point_id}")
     public ResponseEntity getPoint(@PathVariable("username") String username,
-                                   @PathVariable("point_id") Long pointId){
-        return userRepository.findByUsername(username).getPoints();
-    }*/
+                                   @PathVariable("point_id") Long pointId,
+                                   Authentication auth){
+        return doAccess(username, auth, new MakeResponse() {
+            public ResponseEntity make() {
+                return ok(userRepository.findByUsername(username).getPoints().stream().
+                        filter(i -> i.getId().compareTo(pointId) == 0).collect(Collectors.toList()).get(0));
+            }
+        });
+    }
 }
--- backend/src/main/java/com/itmo/lab4/backend/database/PointRepository.java
+++ backend/src/main/java/com/itmo/lab4/backend/database/PointRepository.java
@@ -1,9 +1,9 @@
 package com.itmo.lab4.backend.database;
 
 import com.itmo.lab4.backend.database.entities.PointEntity;
+import com.itmo.lab4.backend.database.entities.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface PointRepository extends JpaRepository<PointEntity, Long> {
-
 
 }
